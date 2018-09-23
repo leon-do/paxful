fe5045cae4b8053389450b59424517d9fa85f850
leondo <leondo123@gmail.com>
2018-09-23 13:34:07 -0400

    uncomments

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const paxful = require('./paxful')
 
 init()
 async function init() {
-    // const tradeList = await paxful.tradeList()
-    // console.log(tradeList)
+    const tradeList = await paxful.tradeList()
+    console.log(tradeList)
 
-    // const userInfo = await paxful.userInfo('ldo')
-    // console.log(userInfo)
+    const userInfo = await paxful.userInfo('ldo')
+    console.log(userInfo)
 
-    // const tradeChatGet = await paxful.tradeChatGet('vMoJN5Db8ox')
-    // console.log(tradeChatGet)
+    const tradeChatGet = await paxful.tradeChatGet('vMoJN5Db8ox')
+    console.log(tradeChatGet)
 
     const tradeChatPost = await paxful.tradeChatPost('vMoJN5Db8ox', 'hello world')
     console.log(tradeChatPost)
